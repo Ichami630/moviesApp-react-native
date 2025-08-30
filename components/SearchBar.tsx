aa0b2c29e64c4b15208873bb4bfbdba6e9416844
ichami630 <brandonichami@gmail.com>
2025-08-30 22:01:23 +0100

    home search bar-mini

---
+++ components/SearchBar.tsx
@@ -0,0 +1,25 @@
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, TextInput, View } from 'react-native';
+
+interface Props {
+    placeholder: string;
+    onPress?: () => void;
+}
+
+const SearchBar = ({placeholder,onPress}:Props) => {
+  return (
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+        <Image source={icons.search} className="size-5 tint-dark-100" resizeMode='contain' tintColor="#ab8bff"/>
+        <TextInput
+            placeholder={placeholder}
+            placeholderTextColor="#a8b5db"
+            className='flex-1 ml-2 text-white font-medium'
+            onPress={onPress}
+            onChangeText={() => {}}
+        />
+    </View>
+  )
+}
+
+export default SearchBar
