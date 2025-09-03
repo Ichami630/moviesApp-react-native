ec81184ce720d286f999759278654e165472d450
ichami630 <brandonichami@gmail.com>
2025-09-03 16:41:25 +0100

    safe area for notches and other safe screen areas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,16 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "./global.css";
 export default function RootLayout() {
   return( 
-    <>
-      <StatusBar hidden={true} />
+    <SafeAreaProvider>
+      {/* <StatusBar hidden={true} /> */}
+      <StatusBar barStyle="light-content" />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
       </Stack>
-    </>
+    </SafeAreaProvider>
   )
 }
